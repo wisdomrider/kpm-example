27e1b84d618aa16097bc329444e2ed836374ef55
Avishek Adhikari <avishekzone@gmail.com>
2020-04-27 19:02:32 +0545

    new module added

--- examples/ip.js
+++ examples/ip.js
@@ -16,15 +16,30 @@ function ip() {
     })
 }
 
+function query(options) { //options contains all the args with key value // here we get {ip:'userinput'}
+    kivia.request("http://ip-api.com/json/" + options.ip, {
+        type: 'GET',
+        prefix: `Fetching information of ${options.ip}.`,
+        callback: (data) => {
+            delete data.status;
+            data.ip = data.query;
+            delete data.query;
+            kivia.makeItem(data);
+        }
+    })
+
+}
+
 const info = {
     name: "My Ip",
     description: "MyIP gives information about your current ip address.\ntype [kpm myip] to get information about your ip.",
-    version: 1,
+    version: 2,
     alias: 'myip',
     author: "wisdomrider",
     email: "avishekzone@gmail.com",
     methods: {
-        undefined: {func: ip, args: [], exclude: true, help: [""]}
+        undefined: {func: ip, args: [], exclude: true, help: [""]},
+        query: {func: query, args: ["ip"], help: ["This command helps you to query a ip.", "IP Address"]}
     }
 };
 module.exports = info;
--- examples/weather.js
+++ examples/weather.js
@@ -1,9 +1,100 @@
 #!/usr/bin/env node
 const kivia = require("kiviamodules");
+const print = (a) => console.log(a)
+const enquirer = require("enquirer");
+const chalk = require("chalk");
+const db = kivia.getDb("weather")
 
+function getDefaultLocation(cb) {
+    const checkLocation = db.get('location', null)
+    if (checkLocation) cb(checkLocation)
+    else {
+        enquirer.prompt({
+            type: 'input',
+            name: 'location',
+            message: 'Enter your home location'
+        }).then(location => {
+            db.set('location', location.location)
+            cb(location.location)
+        })
+    }
+}
 
-function ip() {
+function getKey(cb) {
+    const checkToken = db.get("apiKey", null);
+    if (checkToken) cb(checkToken);
+    else {
+        print(chalk.red("Oh oh ! looks like you have not added a key yet.Get it from \n") + chalk.green("https://openweathermap.org/home/sign_up"))
+        enquirer.prompt(
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Paste your api key here.'
+            }
+        ).then(s => {
+            db.set('apiKey', s.key)
+            cb(s.key)
+        })
+    }
+}
 
+function weather() {
+    getKey((key) => {
+        getDefaultLocation((location) => {
+            kivia.request(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=` + key, {
+                method: 'GET',
+                prefix: `Fetching weather of ${location} .`,
+                callback: (data) => {
+                    parseLocation(data);
+                }, errorcallBack: (e) => {
+                    if (e.isAxiosError && !e.response) {
+                        console.log(chalk.red("  Error : Please check your internet connection."))
+                    } else {
+                        console.log(chalk.red("  Error : " + e.response.data.message) + "\n" + chalk.yellowBright("Note : if you have just created the key then it may take some time to be work or reenter key by typing [kpm weather regenerate]."));
+                    }
+                }
+            })
+
+        })
+
+    })
+}
+
+function parseLocation(json) {
+    console.log(json);
+}
+
+function regenerate() {
+    db.set('apiKey', null);
+    getKey((key) => {
+        console.log(chalk.green("Api key updated successfully."))
+    });
+}
+
+function renewLocation() {
+    db.set('location', null);
+    getDefaultLocation((key) => {
+        console.log(chalk.green("Default location updated successfully."))
+    });
+}
+
+function query(opts) {
+    getKey(key => {
+        kivia.request(`https://api.openweathermap.org/data/2.5/weather?q=${opts.location}&appid=` + key, {
+            method: 'GET',
+            prefix: `Fetching weather of ${opts.location} .`,
+            callback: (data) => {
+                parseLocation(data);
+            }, errorcallBack: (e) => {
+                if (e.isAxiosError && !e.response) {
+                    console.log(chalk.red("  Error : Please check your internet connection."))
+                } else {
+                    console.log(chalk.red("  Error : " + e.response.data.message) + "\n" + chalk.yellowBright("Note : if you have just created the key then it may take some time to be work or reenter key by typing [kpm weather regenerate]."));
+                }
+            }
+        })
+
+    })
 }
 
 const info = {
@@ -14,7 +105,19 @@ const info = {
     author: "wisdomrider",
     email: "avishekzone@gmail.com",
     methods: {
-        default: {func: ip, args: [], help: [""]}
+        undefined: {func: weather, exclude: true, args: [], help: [""]},
+        regenerate: {func: regenerate, args: [], help: ["To enter key again"]},
+        renewlocation: {
+            func: renewLocation,
+            args: [],
+            help: ["To enter your default location again"]
+        },
+        query: {
+            func: query,
+            args: ['location'],
+            help: ["To fetch weather of a specific location", "Location Name"]
+        }
+
     }
 };
 module.exports = info;
